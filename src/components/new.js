c32646b6f5c9450004b683b058c1886026dc123b
= <=>
2020-09-25 22:21:22 +0900

    add new blog function

--- src/components/new.js
+++ src/components/new.js
@@ -1,10 +1,112 @@
 import React, { Component } from 'react';
 
 export default class NewBlog extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeContent = this.onChangeContent.bind(this);
+    this.onChangeCreatedAt = this.onChangeCreatedAt.bind(this);
+    this.onChangeUpdatedAt = this.onChangeUpdatedAt.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangePictureId = this.onChangePictureId.bind(this);
+    this.onChangeStatus = this.onChangeStatus.bind(this);
+
+    this.state = {
+      title: '',
+      content: '',
+      created_at: '',
+      updated_at: '',
+      description: '',
+      picture_id: '',
+      status: false
+    }
+  }
+
+  onChangeTitle(e) {
+    this.setState({
+      title: e.target.value
+    });
+  }
+
+  onChangeContent(e) {
+    this.setState({
+      content: e.target.value
+    });
+  }
+
+  onChangeCreatedAt(e) {
+    this.setState({
+      created_at: e.target.value
+    });
+  }
+
+  onChangeUpdatedAt(e) {
+    this.setState({
+      updated_at: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangePictureId(e) {
+    this.setState({
+      picture_id: e.target.value
+    });
+  }
+
+  onChangeStatus(e) {
+    this.setState({
+      status: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    //Debug
+    console.log(`Form submitted:`);
+    console.log(`Title: ${this.state.title}`);
+    console.log(`Content: ${this.state.content}`);
+
+    this.setState({
+      title: '',
+      content: '',
+      created_at: '',
+      updated_at: '',
+      description: '',
+      picture_id: '',
+      status: false
+    })
+  }
+
+
   render() {
     return (
-      <div>
-        <p>新規作成画面</p>
+      <div style={{merginTop: 10}}>
+        <h3>新規記事作成</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Title: </label>
+            <input type="text" className="form-control" value={this.state.title} onChange={this.onChangeTitle} />
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input type="text" className="form-control" value={this.state.description} onChange={this.onChangeDescription} />
+          </div>
+          <div className="form-group">
+            <label>Content: </label>
+            <input type="textarea" className="form-control" value={this.state.content} onChange={this.onChangeContent} />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="記事投稿" className="btn btn-primary" />
+          </div>
+        </form>
       </div>
     )
   }
