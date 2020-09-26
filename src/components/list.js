eb4d01b8ad0d5d042489d9ef7c3b596aa3864133
= <=>
2020-09-26 23:17:41 +0900

    fixed list.js

--- src/components/list.js
+++ src/components/list.js
@@ -19,7 +19,7 @@ export default class Blogs extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {todos: []};
+    this.state = {blogs: []};
   }
 
   componentDidMount() {
