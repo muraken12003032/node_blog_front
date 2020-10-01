caad4576c603967478c6b6296351ecd8011f0996
= <=>
2020-10-01 23:07:27 +0900

    add link2

--- src/components/blog.js
+++ src/components/blog.js
@@ -4,7 +4,25 @@ const config = require('../config');
 const url = config.backend_url + ':' + config.port;
 
 export default class Blog extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {blog: {}};
+  }
+
+  componentDidMount() {
+    axios.get(url + this.props.match.url)
+      .then(response => {
+        this.setState({ blog: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
+
   render() {
+    console.log(this.props);
     return(
       <div>
         ここにブログ記事を表示
