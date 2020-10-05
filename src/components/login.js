a467d878b12d49a613a7fc0a4c358ae7fa4870fe
= <=>
2020-10-05 22:12:10 +0900

    add onchange action

--- src/components/login.js
+++ src/components/login.js
@@ -4,6 +4,9 @@ export default class Login extends Component {
   constructor(props) {
     super(props);
 
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+
     this.state = {
       email: '',
       password: ''
@@ -15,17 +18,29 @@ export default class Login extends Component {
     console.log(this.state);
   }
 
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.password
+    })
+  }
+
   render() {
     return(
       <div>
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>Email: </label>
-            <input type="text" className="form-control" value={this.state.email} />
+            <input type="text" className="form-control" value={this.state.email} onChange={this.onChangeEmail} />
           </div>
           <div className="form-group">
             <label>Password: </label>
-            <input type="password" className="form-control" value={this.state.password} />
+            <input type="password" className="form-control" value={this.state.password} onChange={this.onChangePassword} />
           </div>
           <div className="form-group">
             <input type="submit" value={this.props.submit_value} className="btn btn-primary" />
